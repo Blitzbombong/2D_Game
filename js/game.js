d554772a08ec816298b30d8aefb82f3b19b0ec06
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-01 20:57:55 +0100

    added models and chansh game.js

--- js/game.js
+++ js/game.js
@@ -5,7 +5,4 @@ let character = new Image();
 function init() {
     canvas = document.getElementById("myCanvas");
     ctx = canvas.getContext("2d");
-    character.src = "../img/2_character_pepe/2_walk/W-21.png";
-
-    ctx.drawImage(character, 20, 20, 50, 150);
 }
