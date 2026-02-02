8af48e1f3f959ede36ba3758bdfcae65f829b737
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-02 19:49:07 +0100

    added requestAnimationFrame() and clearRect()

--- models/world.class.js
+++ models/world.class.js
@@ -7,6 +7,7 @@ class World {
         new Chicken()
     ];
 
+    canvas;
     ctx;
 
     constructor(canvas) {
@@ -19,9 +20,14 @@ class World {
 
 
     draw() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
         this.enemies.forEach((enemy) => {
             this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
         });
+
+        // drow() wird immer wieder aufgerufen
+        requestAnimationFrame(() => this.draw());
     }
 }
