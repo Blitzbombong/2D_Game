d08bf1ccd66e624a69b54e6561556bd5b2a42e33
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-02 19:57:12 +0100

    added Math.random() to Chicken

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -5,6 +5,9 @@ class Chicken extends MovableObject {
     constructor() {
         super();
         this.loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+
+        this.x = 200 + Math.random() * 500; // Zufällige Startposition
+        this.y = 350; // Bodenposition
     }
 
 }
