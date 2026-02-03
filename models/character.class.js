58e5ef7ab349f7625763ebf4fde4f92c1c0323c7
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-03 21:17:10 +0100

    added loadImages() function

--- models/character.class.js
+++ models/character.class.js
@@ -1,12 +1,18 @@
 class Character extends MovableObject {
     height = 230
     width = 120
-
+    y = 195; // Bodenposition
 
     constructor() {
-        super();
-        this.loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
-        this.y = 195; // Bodenposition
+        super();this.loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages([
+            'img/2_character_pepe/2_walk/W-21.png',
+            'img/2_character_pepe/2_walk/W-22.png',
+            'img/2_character_pepe/2_walk/W-23.png',
+            'img/2_character_pepe/2_walk/W-24.png',
+            'img/2_character_pepe/2_walk/W-25.png',
+            'img/2_character_pepe/2_walk/W-26.png'
+        ]);
     }
 
 
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,10 +4,26 @@ class MovableObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
+
 
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
+    }
+
+
+    /**
+     * Loads an array of images into the imageCache object.
+     * Each image is stored with its path as the key.
+     * @param {Array<string>} arr - An array of image paths.
+     */
+    loadImages(arr) {
+        arr.forEach(path => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = path;
+        });
     }
 
 
