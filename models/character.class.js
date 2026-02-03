1574c777163a12348915a676ee88d9fa084f68d7
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-03 21:34:17 +0100

    animate Character

--- models/character.class.js
+++ models/character.class.js
@@ -2,17 +2,29 @@ class Character extends MovableObject {
     height = 230
     width = 120
     y = 195; // Bodenposition
-
-    constructor() {
-        super();this.loadImage('img/2_character_pepe/2_walk/W-21.png');
-        this.loadImages([
+    IMAGES_WALKING = [
             'img/2_character_pepe/2_walk/W-21.png',
             'img/2_character_pepe/2_walk/W-22.png',
             'img/2_character_pepe/2_walk/W-23.png',
             'img/2_character_pepe/2_walk/W-24.png',
             'img/2_character_pepe/2_walk/W-25.png',
             'img/2_character_pepe/2_walk/W-26.png'
-        ]);
+    ];
+    currentImage = 0;
+
+    constructor() {
+        super();this.loadImage('img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
+    }
+
+
+    animate() {
+        setInterval(() => {
+            let path = this.IMAGES_WALKING[this.currentImage];
+            this.img = this.imageCache[path];
+            this.currentImage = (this.currentImage + 1) % this.IMAGES_WALKING.length;
+        }, 100);
     }
 
 
