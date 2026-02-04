d744786f1040801fb0243687e03abbd7f37b1360
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-04 19:44:08 +0100

    added keyPressEvent() funktion to Keyboard

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,11 +1,44 @@
 class Keyboard {
-    LEFT;
-    LEFT;
-    RIGHT;
-    UP;
-    DOWN;
-    SPACE;
+    LEFT = false;
+    RIGHT = false;
+    SPACE = false;
+    S = false;
     constructor() {
-        
+        this.keyPressEvent();
+    }
+
+    keyPressEvent() {
+        // Wenn die Taste gedrückt wird wird die entsprechende Variable true
+       window.addEventListener("keydown", (e) => {
+            if (e.code === "ArrowLeft"){
+            this.LEFT = true;
+        }
+         if (e.code === "ArrowRight"){
+            this.RIGHT = true;
+        }
+         if (e.code === "Space"){
+            this.SPACE = true;
+        } else
+         if (e.code === "KeyS"){
+            this.S = true;
+        }
+       });
+       
+
+       // Wenn die Taste losgelassen wird wird die entsprechende Variable false
+       window.addEventListener("keyup", (e) => {
+        if (e.code === "ArrowLeft"){
+            this.LEFT = false;
+        }
+         if (e.code === "ArrowRight"){
+            this.RIGHT = false;
+        }
+         if (e.code === "Space"){
+            this.SPACE = false;
+        } else
+         if (e.code === "KeyS"){
+            this.S = false;
+        }
+       });
     }
 }
