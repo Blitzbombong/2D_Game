a67b7bbabeaf6d7ddaeaadcc33e4c5de0dca0f04
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-09 20:10:00 +0100

    added bottle, coin, health and ensboss bar

---
+++ models/coin-bar.class.js
@@ -0,0 +1,10 @@
+class CoinBar extends StatusBar {
+    IMAGES = [
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/0.png',   
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/20.png',
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/40.png',
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/60.png',
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/80.png',
+        'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/100.png'
+    ];  
+}
---
+++ models/endboss-bar.class.js
@@ -0,0 +1,10 @@
+class EndbossBar extends StatusBar {
+    IMAGES = [
+        'img/7_statusbars/1_statusbar/4_statusbar_endboss/blue/0.png',
+        'img/7_statusbars/1_statusbar/4_statusbar_endboss/blue/20.png',
+        'img/7_statusbars/1_statusbar/4_statusbar_endboss/blue/40.png',
+        'img/7_statusbars/1_statusbar/4_statusbar_endboss/blue/60.png',
+        'img/7_statusbars/1_statusbar/4_statusbar_endboss/blue/80.png',
+        'img/7_statusbars/1_statusbar/4_statusbar_endboss/blue/100.png'
+    ];  
+}
