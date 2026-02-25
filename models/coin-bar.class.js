d14e7c06e84c6c21d9227946afebf86d68f0643b
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-25 20:42:59 +0100

    updated coin class and coin-bar and added JS.Doc

--- models/coin-bar.class.js
+++ models/coin-bar.class.js
@@ -1,20 +1,20 @@
 class CoinBar extends StatusBar {
-    IMAGES = [
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/0.png',   
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/20.png',
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/40.png',
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/60.png',
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/80.png',
-        'img/7_statusbars/1_statusbar/1_statusbar_coin/orange/100.png'
-    ];  
+  IMAGES = [
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/orange/0.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/orange/20.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/orange/40.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/orange/60.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/orange/80.png",
+    "img/7_statusbars/1_statusbar/1_statusbar_coin/orange/100.png",
+  ];
 
-    constructor() {
-        super();
-        this.loadImages(this.IMAGES);
-        this.x = 20;
-        this.y = 40;
-        this.width = 150;
-        this.height = 35;
-        this.setPercentage(0);
-    }
+  constructor() {
+    super();
+    this.loadImages(this.IMAGES);
+    this.x = 20;
+    this.y = 40;
+    this.width = 150;
+    this.height = 35;
+    this.setPercentage(0);
+  }
 }
--- models/coins.class.js
+++ models/coins.class.js
@@ -1,31 +1,37 @@
 class Coins extends MovableObject {
-    width = 80;
-    height = 80;
-    y = 320;
-    offset = {
-        top: 25,
-        left: 25, 
-        right: 25, 
-        bottom: 25
-     }
+  width = 80;
+  height = 80;
+  y = 320;
+  offset = {
+    top: 25,
+    left: 25,
+    right: 25,
+    bottom: 25,
+  };
 
-    IMAGES_COINS = [
-        'img/8_coin/coin_1.png',
-        'img/8_coin/coin_2.png'
-    ]
+  IMAGES_COINS = ["img/8_coin/coin_1.png", "img/8_coin/coin_2.png"];
 
-    constructor(x) {
-        super();
-        this.loadImage(this.IMAGES_COINS[0]);
-        this.loadImages(this.IMAGES_COINS);
-        this.y = 60 + Math.random() * 150;
-        this.x = x;
-        this.animate();
-    }
+  constructor(x) {
+    super();
+    this.loadImage(this.IMAGES_COINS[0]);
+    this.loadImages(this.IMAGES_COINS);
+    this.y = 60 + Math.random() * 150;
+    this.x = x;
+    this.animate();
+  }
 
-    animate() {
-        setStoppableInterval(() => {
-            this.playAnimation(this.IMAGES_COINS);
-        }, 300);
-    }
+  /**
+   * Starts the animation loop for the coin's blinking effect.
+   */
+  animate() {
+    setStoppableInterval(() => this.handleAnimation(), 300);
+  }
+
+  /**
+   * Cycles through the coin images to create a glimmering effect.
+   */
+  handleAnimation() {
+    this.playAnimation(this.IMAGES_COINS);
+  }
 }
+
