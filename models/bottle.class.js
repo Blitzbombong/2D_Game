54f98db619b01e1e88539613df5456ad64c35c45
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-25 19:38:36 +0100

    class Bottle updated and added JS.Doc

--- models/bottle.class.js
+++ models/bottle.class.js
@@ -1,34 +1,34 @@
-class Bottle extends MovableObject {    
-    height = 60;
-    width = 50;
-    y = 360;
+class Bottle extends MovableObject {
+  height = 60;
+  width = 50;
+  y = 360;
+  offset = {
+    top: 10,
+    left: 15,
+    right: 10,
+    bottom: 5,
+  };
 
-    offset = {
-        top: 10,
-        left: 15,
-        right: 10,
-        bottom: 5
-    }
+  IMAGES_BOTTLE = [
+    "img/6_salsa_bottle/1_salsa_bottle_on_ground.png",
+    "img/6_salsa_bottle/2_salsa_bottle_on_ground.png",
+  ];
 
-    IMAGES_BOTTLE = [
-        'img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
-        'img/6_salsa_bottle/2_salsa_bottle_on_ground.png'
-    ];
+  /**
+   * Creates a new Bottle object.
+   * @param {number} x - the x position of the bottle
+   */
+  constructor(x) {
+    super();
+    this.loadImage(this.IMAGES_BOTTLE[0]);
+    this.loadImages(this.IMAGES_BOTTLE);
+    this.x = x;
+    this.animate();
+  }
 
-    
-
-    constructor(x) {
-        super();
-        this.loadImage(this.IMAGES_BOTTLE[0]);
-        this.loadImages(this.IMAGES_BOTTLE);
-        this.x = x;
-        this.animate();
-        
-    }
-
-    animate() {
-        setStoppableInterval(() => {
-            this.playAnimation(this.IMAGES_BOTTLE);
-        }, 500);
-    }
+  animate() {
+    setStoppableInterval(() => {
+      this.playAnimation(this.IMAGES_BOTTLE);
+    }, 500);
+  }
 }
