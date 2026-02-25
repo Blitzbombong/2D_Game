73f987ad4e239840d162489a9a02663f32784dca
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-25 20:25:47 +0100

    class Chicken updated and added JS.Doc

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,70 +1,85 @@
 class Chicken extends MovableObject {
-    height = 70;
-    width = 60;
-    x = 400 + Math.random() * 1800; // Zufällige Startposition
-    y = 352; // Bodenposition
-    currentImage = 0;
-    speed = 0.15 + Math.random() * 0.5;
-    isDead = false;
+  height = 70;
+  width = 60;
+  y = 352;
+  speed = 0.15 + Math.random() * 0.5;
+  isDead = false;
 
-    offset = {
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 5
-    }
+  offset = { 
+    top: 0,
+    left: 0, 
+    right: 0, 
+    bottom: 5 
+};
 
-    IMAGES_WALKING = [
-        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
-        'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
-        'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
-    ]
+  IMAGES_WALKING = [
+    "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
+    "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+  ];
 
-    IMAGES_DEAD = [
-        'img/3_enemies_chicken/chicken_normal/2_dead/dead.png'
-    ];
-   
+  IMAGES_DEAD = ["img/3_enemies_chicken/chicken_normal/2_dead/dead.png"];
 
-    constructor() {
-        super();
-        this.loadImage(this.IMAGES_WALKING[0]);
-        this.loadImages(this.IMAGES_WALKING);
-        this.loadImages(this.IMAGES_DEAD);
-        this.animate();
-    }
+  constructor() {
+    super();
+    this.x = 400 + Math.random() * 1800; // Startposition zufällig
+    this.loadImage(this.IMAGES_WALKING[0]);
+    this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_DEAD);
+    this.animate();
+  }
 
-   animate() {
-    // 1. Intervall: Alles was mit POSITION zu tun hat (Bewegung)
-    setStoppableInterval(() => {
-        if (!this.isDead) {
-            this.moveLeft();
-            
-        }
-        
-        // Deine Logik: Wenn das Huhn links rausläuft, kommt es rechts wieder rein
-        if (this.x < -this.width) {
-            this.x = 1700 + Math.random() * 500; // Ein bisschen Zufall macht es natürlicher
-        }
-    }, 1000 / 60);
+  /**
+   * Orchestrates the movement and animation loops for the chicken.
+   */
+  animate() {
+    setStoppableInterval(() => this.handleMovement(), 1000 / 60);
+    setStoppableInterval(() => this.handleAnimation(), 150);
+  }
 
-    // 2. Intervall: Alles was mit AUSSEHEN zu tun hat (Animation)
-    setStoppableInterval(() => {
-        if (this.isDead) {
-            this.loadImage(this.IMAGES_DEAD[0]); // Sofort das flache Bild zeigen
-        } else {
-            this.playAnimation(this.IMAGES_WALKING); // Nutzt die Logik aus MovableObject
-        }
-    }, 150); // 150ms sieht deutlich flüssiger aus!
-}
-
-
-    die() {
-        this.isDead = true;
-        this.speed = 0;
-    }
-
-
+  /**
+   * Handles the horizontal movement and screen wrapping.
+   */
+  handleMovement() {
+    if (this.isDead) return;
     
+    this.moveLeft();
+    if (this.isOffScreen()) {
+      this.respawn();
+    }
+  }
 
+  /**
+   * Checks if the chicken has moved completely off the left side of the screen.
+   * @returns {boolean}
+   */
+  isOffScreen() {
+    return this.x < -this.width;
+  }
 
+  /**
+   * Places the chicken back to the right side of the level at a random position.
+   */
+  respawn() {
+    this.x = 1700 + Math.random() * 500;
+  }
+
+  /**
+   * Manages the visual state of the chicken (walking or dead).
+   */
+  handleAnimation() {
+    if (this.isDead) {
+      this.loadImage(this.IMAGES_DEAD[0]);
+    } else {
+      this.playAnimation(this.IMAGES_WALKING);
+    }
+  }
+
+  /**
+   * Sets the chicken to dead state and stops its movement.
+   */
+  die() {
+    this.isDead = true;
+    this.speed = 0;
+  }
 }
