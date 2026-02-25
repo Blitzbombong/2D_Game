9212cbb7b575de0edb81cfde5c570ad606831089
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-25 19:36:40 +0100

    class BottleBar updated and added JS.Doc

--- models/bottle-bar.class.js
+++ models/bottle-bar.class.js
@@ -1,20 +1,27 @@
 class BottleBar extends StatusBar {
-    IMAGES = [
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png',
-        'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png'
-    ];  
+  IMAGES = [
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png",
+    "img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png",
+  ];
 
-    constructor() {
-        super();
-        this.loadImages(this.IMAGES);
-        this.x = 20;
-        this.y = 70;
-        this.width = 150;
-        this.height = 35;
-        this.setPercentage(0);
-    }
+  /**
+   * Initializes a new BottleBar object.
+   * This function calls the superclass constructor and sets the
+   * x, y, width, height and percentage of the object.
+   * The images for the object are also loaded into the imageCache.
+   * @see StatusBar
+   */
+  constructor() {
+    super();
+    this.loadImages(this.IMAGES);
+    this.x = 20;
+    this.y = 70;
+    this.width = 150;
+    this.height = 35;
+    this.setPercentage(0);
+  }
 }
