5d222d276412ad71b8510e5d929185db9f4dc325
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-25 19:33:50 +0100

    class BackgroundObject updated and added JS.Doc

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,13 +1,19 @@
 class BackgroundObject extends MovableObject {
-    height = 480;
-    width = 720;
-    y = 0;  // Bodenposition    
-    x = 0;  // Bodenposition  
-    
-    constructor(imagePath, x, y) {
-        super();
-        this.loadImage(imagePath); // Nutzt den Pfad, den du im Array übergeben hast
-        this.x = x;
-        this.y = y;
-    }
+  height = 480;
+  width = 720;
+  y = 0; // Bodenposition
+  x = 0; // Bodenposition
+
+  /**
+   * Creates a new BackgroundObject.
+   * @param {string} imagePath - the path to the image of the object
+   * @param {number} x - the x position of the object
+   * @param {number} y - the y position of the object
+   */
+  constructor(imagePath, x, y) {
+    super();
+    this.loadImage(imagePath); // Nutzt den Pfad, den du im Array übergeben hast
+    this.x = x;
+    this.y = y;
+  }
 }
