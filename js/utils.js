e214a450070ce82c26c45ca5e43b3f23cf5dd09f
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-25 18:17:35 +0100

    Funktion untils.js sauber gemacht und JSDoc Standrart eingefügt

--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,25 @@
-let intervalIds = []; // Array, um alle Interval-IDs zu speichern
+let intervalIds = [];
 
-
+/**
+ * Sets a interval that can be stopped later
+ * @param {function} fn - the function to be called
+ * @param {number} time - the time between calls to fn in milliseconds
+ * @returns {number} the ID of the interval that can be stopped later
+ */
 function setStoppableInterval(fn, time) {
-    let id = setInterval(fn, time);
-    intervalIds.push(id);
-    return id;
+  let id = setInterval(fn, time);
+  intervalIds.push(id);
+  return id;
 }
 
-
+/**
+ * Stops all currently running intervals
+ * @description
+ * This function stops all intervals that have been set with setStoppableInterval.
+ * It does this by calling clearInterval on each ID in the intervalIds array.
+ * After stopping all intervals, the array is also cleared to prevent memory leaks.
+ */
 function stopGame() {
-    // Alle Intervalle stoppen
-    intervalIds.forEach(id => clearInterval(id));
-    intervalIds = []; // Array leeren
+  intervalIds.forEach((id) => clearInterval(id));
+  intervalIds = [];
 }
