d98152baadc7f324ef586476b53c32308d34f4ae
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-26 18:32:10 +0100

    added JS.Doc

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,87 +1,86 @@
 class Keyboard {
-    LEFT = false;
-    RIGHT = false;
-    SPACE = false;
-    S = false;
-    constructor() {
-        this.keyPressEvent();
-    }
+  LEFT = false;
+  RIGHT = false;
+  SPACE = false;
+  S = false;
+  constructor() {
+    this.keyPressEvent();
+  }
 
-    keyPressEvent() {
-        // Wenn die Taste gedrückt wird wird die entsprechende Variable true
-       window.addEventListener("keydown", (e) => {
-            if (e.code === "ArrowLeft"){
-            this.LEFT = true;
-        }
-         if (e.code === "ArrowRight"){
-            this.RIGHT = true;
-        }
-         if (e.code === "Space"){
-            this.SPACE = true;
-        } else
-         if (e.code === "KeyS"){
-            this.S = true;
-        }
-       });
-       
+  /**
+   * Adds event listeners for the keyboard events of the keys left, right, space and S.
+   * The event listeners set the corresponding properties of the Keyboard object to true when the key is pressed down and false when the key is released.
+   */
+  keyPressEvent() {
+    window.addEventListener("keydown", (e) => {
+      if (e.code === "ArrowLeft") {
+        this.LEFT = true;
+      }
+      if (e.code === "ArrowRight") {
+        this.RIGHT = true;
+      }
+      if (e.code === "Space") {
+        this.SPACE = true;
+      } else if (e.code === "KeyS") {
+        this.S = true;
+      }
+    });
 
-       // Wenn die Taste losgelassen wird wird die entsprechende Variable false
-       window.addEventListener("keyup", (e) => {
-        if (e.code === "ArrowLeft"){
-            this.LEFT = false;
-        }
-         if (e.code === "ArrowRight"){
-            this.RIGHT = false;
-        }
-         if (e.code === "Space"){
-            this.SPACE = false;
-        } else
-         if (e.code === "KeyS"){
-            this.S = false;
-        }
-       });
-    }
+    window.addEventListener("keyup", (e) => {
+      if (e.code === "ArrowLeft") {
+        this.LEFT = false;
+      }
+      if (e.code === "ArrowRight") {
+        this.RIGHT = false;
+      }
+      if (e.code === "Space") {
+        this.SPACE = false;
+      } else if (e.code === "KeyS") {
+        this.S = false;
+      }
+    });
+  }
 
+  /**
+   * Binds touch events to the keyboard object. Listeners are added to the left, right, space and S buttons on the mobile screen.
+   * When a touch event is triggered, the corresponding property of the Keyboard object is set to true or false.
+   * This function is used to enable mobile controls for the game.
+   */
+  bindTouchEvents() {
+    document.getElementById("btn-left").addEventListener("touchstart", (e) => {
+      e.preventDefault(); // Verhindert nerviges Zoomen/Scrollen am Handy
+      this.LEFT = true;
+    });
+    document.getElementById("btn-left").addEventListener("touchend", (e) => {
+      e.preventDefault();
+      this.LEFT = false;
+    });
 
-    bindTouchEvents() {
-        // LINKS
-        document.getElementById('btn-left').addEventListener('touchstart', (e) => {
-            e.preventDefault(); // Verhindert nerviges Zoomen/Scrollen am Handy
-            this.LEFT = true;
-        });
-        document.getElementById('btn-left').addEventListener('touchend', (e) => {
-            e.preventDefault();
-            this.LEFT = false;
-        });
+    document.getElementById("btn-right").addEventListener("touchstart", (e) => {
+      e.preventDefault();
+      this.RIGHT = true;
+    });
+    document.getElementById("btn-right").addEventListener("touchend", (e) => {
+      e.preventDefault();
+      this.RIGHT = false;
+    });
 
-        // RECHTS
-        document.getElementById('btn-right').addEventListener('touchstart', (e) => {
-            e.preventDefault();
-            this.RIGHT = true;
-        });
-        document.getElementById('btn-right').addEventListener('touchend', (e) => {
-            e.preventDefault();
-            this.RIGHT = false;
-        });
+    document.getElementById("btn-jump").addEventListener("touchstart", (e) => {
+      e.preventDefault();
+      this.SPACE = true;
+    });
+    document.getElementById("btn-jump").addEventListener("touchend", (e) => {
+      e.preventDefault();
+      this.SPACE = false;
+    });
 
-        // SPRINGEN
-        document.getElementById('btn-jump').addEventListener('touchstart', (e) => {
-            e.preventDefault();
-            this.SPACE = true;
-        });
-        document.getElementById('btn-jump').addEventListener('touchend', (e) => {
-            e.preventDefault();
-            this.SPACE = false;
-        });
-
-        // WERFEN
-        document.getElementById('btn-throw').addEventListener('touchstart', (e) => {
-            e.preventDefault();
-            this.S = true; // Oder welche Taste du für Werfen nutzt
-        });
-        document.getElementById('btn-throw').addEventListener('touchend', (e) => {
-            e.preventDefault();
-            this.S = false;
-        });
-    }
+    document.getElementById("btn-throw").addEventListener("touchstart", (e) => {
+      e.preventDefault();
+      this.S = true; // Oder welche Taste du für Werfen nutzt
+    });
+    document.getElementById("btn-throw").addEventListener("touchend", (e) => {
+      e.preventDefault();
+      this.S = false;
+    });
+  }
 }
