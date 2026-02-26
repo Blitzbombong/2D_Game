c00cd5ef7e70e430f03351954ff276f3e15b60a9
Waldemar Giesbrecht <128844779+Blitzbombong@users.noreply.github.com>
2026-02-26 18:27:57 +0100

    updated class DrawableObject and added JS.Doc

--- models/drawable_object.class.js
+++ models/drawable_object.class.js
@@ -1,33 +1,39 @@
 class DrawableObject {
+  height = 150;
+  width = 100;
+  x = 120;
+  y = 250;
+  img;
+  imageCache = {};
+  currentImage = 0;
 
-    height = 150;
-    width = 100;
-    x = 120;
-    y = 250;
-    img;
-    imageCache = {};
-    currentImage = 0;
-    draw(ctx) {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
+  /**
+   * Draws the object on the canvas.
+   * @param {CanvasRenderingContext2D} ctx - the context to draw on
+   */
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
 
+  /**
+   * Loads an image into the imageCache object.
+   * @param {string} path - the path to the image
+   */
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
 
-    loadImage(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-
-    /**
-     * Loads an array of images into the imageCache object.
-     * Each image is stored with its path as the key.
-     * @param {Array<string>} arr - An array of image paths.
-     */
-    loadImages(arr) {
-        arr.forEach(path => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-    }
+  /**
+   * Loads an array of images into the imageCache object.
+   * Each image is stored with its path as the key.
+   * @param {Array<string>} arr - An array of image paths.
+   */
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
+  }
 }
